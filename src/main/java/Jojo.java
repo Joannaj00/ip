6f5e4feda4f12f4af8224251f06218c16c858e93
Joannaj00 <bluejoannaj@gmail.com>
2025-08-28 20:39:28 +0800

    Level-1: Echo

--- src/main/java/Jojo.java
+++ src/main/java/Jojo.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
 public class Jojo {
+    private static final String LINE = "____________________________________________________________";
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Jojo");
-        System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(LINE);
+        System.out.println(" " + "Hello! I'm Jojo\n What can I do for you?");
+        System.out.println(LINE);
+
+        while(true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(LINE);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(LINE);
+                break;
+            }
+            System.out.println(LINE);
+            System.out.println(input);
+            System.out.println(LINE);
+        }
+        sc.close();
     }
 }
